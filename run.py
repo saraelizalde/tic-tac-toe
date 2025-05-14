bacdfdf1e9f2216eb5eba7973cfdb7e244c98563
saraelizalde <j7esg480@students.codeinstitute.net>
2025-05-14 18:05:51 +0100

    Create Board class with __init__ and display_board methods and  main function

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Board :
+    """
+    Creates the board and displays it.
+    """
+    def __init__(self):
+        self.grid = [" " for _ in range(9)]
+
+    def display_board(self):
+        print()
+        for row in range(3):
+            i= row * 3
+            print(f" {self.grid[i]} | {self.grid[i+1]} | {self.grid[i+2]} ")
+            if row < 2:
+                print("---|---|---")
+        print()
+
+
+def main():
+    """
+    Run all program functions
+    """
+    board = Board()
+    board.display_board()
+
+main()
+
+        
